7ee58da32a9bb111fda62581b45781ed759eea4d
Daniel Redly <dredly@gmail.com>
2022-09-28 13:18:12 +0100

    Changed initial letter quantities

--- letters.ts
+++ letters.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { Letter } from './types';
 
-export const allLetters = 'AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYZ';
+export const allLetters = 'AAAAABBCCCCDDDDEEEEEEEEEEEEFFFFGGHHHHHIIIIIJKLLLLLMMMMNNNNNOOOOOOPPPQRRRRRSSSSSTTTTTTTUUUUVVWWWXYYYZ';
 
 export const generateLetters = (startingString: string): Letter[] => {
 	return startingString.split('').map(char => {
