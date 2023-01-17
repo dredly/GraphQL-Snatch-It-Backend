a6f177734b68414ce67ac79c57bc91636af814ac
Daniel Redly <dredly@gmail.com>
2023-01-17 22:43:00 +0000

    Add test for random rotation in letter generation

--- tests/letters.test.ts
+++ tests/letters.test.ts
@@ -1,5 +1,12 @@
 import { generateLetters } from '../letters';
 
-test('generateLetters function', () => {
-	expect(generateLetters('AAAABC')).toHaveLength(6);
+describe('generateLetters function', () => {
+	it('Should generate one letter for each character in the input string', () => {
+		expect(generateLetters('AAAABC')).toHaveLength(6);
+	});
+	it('Should randomise the rotation of the letters it generates', () => {
+		const rotations = generateLetters('AAAAAAA').map(lett => lett.rotation);
+		const numUnique = (new Set(rotations)).size;
+		expect(numUnique).toBeGreaterThan(0);
+	});
 });
