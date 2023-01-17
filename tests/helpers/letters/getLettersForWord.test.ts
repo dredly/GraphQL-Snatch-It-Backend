2578aa06b8b8af2f0c9bac5f87db6e33dd6df296
Daniel Redly <dredly@gmail.com>
2023-01-17 22:32:41 +0000

    Added rotation property to letters and made sure existing tests run

--- tests/helpers/letters/getLettersForWord.test.ts
+++ tests/helpers/letters/getLettersForWord.test.ts
@@ -5,17 +5,17 @@ describe('getLettersForWord function', () => {
 	it('Returns the letters for the word and the remaining word when given valid input', () => {
 		expect(getLettersForWord('riff', testLetterPool)).toEqual({
 			letters: [
-				{id: '8', value: 'R'},
-				{id: '9', value: 'I'},
-				{id: '6', value: 'F'},
-				{id: '7', value: 'F'},
+				{id: '8', value: 'R', rotation: 0},
+				{id: '9', value: 'I', rotation: 0},
+				{id: '6', value: 'F', rotation: 0},
+				{id: '7', value: 'F', rotation: 0},
 			],
 			remaining: [
-				{id: '1', value: 'A'},
-				{id: '2', value: 'B'},
-				{id: '3', value: 'C'},
-				{id: '4', value: 'D'},
-				{id: '5', value: 'E'},
+				{id: '1', value: 'A', rotation: 0},
+				{id: '2', value: 'B', rotation: 0},
+				{id: '3', value: 'C', rotation: 0},
+				{id: '4', value: 'D', rotation: 0},
+				{id: '5', value: 'E', rotation: 0},
 			],
 		});    
 	});
